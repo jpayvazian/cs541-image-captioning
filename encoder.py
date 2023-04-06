17a9cb3d309982b31b09379e5c8bccca3a5d9670
jjlaber <jlaber21@gmail.com>
2023-04-06 11:10:22 -0400

    Update encoder.py

--- encoder.py
+++ encoder.py
@@ -2,19 +2,20 @@ import tensorflow as tf
 import os
 import pickle
 import numpy as np
+from vit_keras import vit, utils
 
 '''
-Feature extraction (encoder) for images using pretrained CNN Resnet
+Feature extraction (encoder) for images using pretrained CNN Resnet or VIT
 Save/load features to .pkl to avoid rerunning this operation
 :param image_files: list of string image file names
 
 :return: dictionary of features for each image
 '''
-def extract_features(image_files):
+def extract_features_resnet(image_files):
     if not os.path.isfile("flickr8k/features.pkl"):
         features = {}
         model = tf.keras.applications.ResNet50(include_top=False, weights='imagenet')
-
+    
         for file in image_files:
             image = tf.keras.utils.load_img(os.path.join('flickr8k/Images', file), target_size=(224, 224))
             image = tf.keras.utils.img_to_array(image)
@@ -35,3 +36,31 @@ def extract_features(image_files):
     return features
 
 # TODO: ViT encoder
+def extract_features_vit(image_files):
+    if not os.path.isfile("flickr8k/features.pkl"):
+        features = {}
+        image_size = 224
+        classes = utils.get_imagenet_classes()
+        model = vit.vit_b16(
+            image_size = image_size,
+            activation ='sigmoid',
+            pretrained=True,
+            include_top=True,
+            pretrained_top=True)
+        
+        for file in image_files:
+            image = utils.read(os.path.join('flickr8k/Images', file), image_size)
+            processed_image = vit.preprocess_inputs(image).reshape(1, image_size, image_size, 3)
+            feature = model.predict(processed_image)
+            features[file] = feature
+            
+        # Cache features to avoid having to run multiple times
+        with open("flickr8k/features.pkl", "wb") as f:
+            pickle.dump(features, f)
+
+    # If features already cached, load them from disk
+    else:
+        with open("flickr8k/features.pkl", "rb") as f:
+            features = pickle.load(f)
+
+    return features
